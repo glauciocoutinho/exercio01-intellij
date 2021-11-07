aa0cdb9dd89f2132f167eae966c610fec32006f5
glauciocoutinho <glauciocoutinho@hotmail.com>
2021-11-07 10:59:14 -0400

    first commit

---
+++ src/com/gcsdata/PrimeiroProgramaJava.java
@@ -0,0 +1,16 @@
+package com.gcsdata;
+
+import com.gcsdata.model.Gato;
+
+public class PrimeiroProgramaJava {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello Again! " + (a+b));*/
+    }
+}
